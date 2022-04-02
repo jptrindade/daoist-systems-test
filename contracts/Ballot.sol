a5d9f00e317e9fa7043ed13ae4f993e4d7b96f8f
João Trindade <joao.trindade@optimile.eu>
2022-04-02 12:08:02 +0100

    feat: Simplified and structured Ballot code

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -1,144 +1,119 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-/// @title Voting with delegation.
+import "./gnosis-safe/GnosisSafe.sol";
+
+/// @title A contract that allows voting on proposals based
+//  on the amount staked on a given Balancer pool.
+//  The proposals determine who can be a Owner of a Gnosis Safe
 contract Ballot {
-    // This declares a new complex type which will
-    // be used for variables later.
-    // It will represent a single voter.
-    struct Voter {
-        uint256 weight; // weight is accumulated by delegation
-        bool voted; // if true, that person already voted
-        address delegate; // person delegated to
+    // This represents the information of a vote.
+    // It is accessed by a mapping that links the address
+    // of the voter to this information.
+    struct VoterInfo {
+        uint256 weight; // weight is accumulated by the stake amount in a Balancer Pool
         uint256 vote; // index of the voted proposal
     }
 
     // This is a type for a single proposal.
     struct Proposal {
-        bytes32 name; // short name (up to 32 bytes)
+        string name; // short name (candidate pseudonym for example)
+        address candidate; // address of the owner candidate
+        bool willBeOwner; // remove from Owner list if false, add to Owner list if true
         uint256 voteCount; // number of accumulated votes
+        bool executed; // check if the Proposal was already executed
     }
 
-    address public chairperson;
+    address public balancerPool;
+
+    address payable public gnosisSafe;
 
     // This declares a state variable that
     // stores a `Voter` struct for each possible address.
-    mapping(address => Voter) public voters;
+    mapping(address => VoterInfo) public voters;
 
     // A dynamically-sized array of `Proposal` structs.
     Proposal[] public proposals;
 
-    /// Create a new ballot to choose one of `proposalNames`.
-    constructor(bytes32[] memory proposalNames) {
-        chairperson = msg.sender;
-        voters[chairperson].weight = 1;
-
-        // For each of the provided proposal names,
-        // create a new proposal object and add it
-        // to the end of the array.
-        for (uint256 i = 0; i < proposalNames.length; i++) {
-            // `Proposal({...})` creates a temporary
-            // Proposal object and `proposals.push(...)`
-            // appends it to the end of `proposals`.
-            proposals.push(Proposal({name: proposalNames[i], voteCount: 0}));
-        }
+    /// Create a new ballot with weights derived from
+    /// the amount of tokens staked in `_balancerPool`.
+    /// @param _balancerPool Address of the Balancer Pool to check the weight of votes
+    /// @param _gnosisSafe Address of the Gnosis Safe to add/remove Owners
+    constructor(address _balancerPool, address payable _gnosisSafe) {
+        balancerPool = _balancerPool;
+        gnosisSafe = _gnosisSafe;
     }
 
-    // Give `voter` the right to vote on this ballot.
-    // May only be called by `chairperson`.
-    function giveRightToVote(address voter) external {
-        // If the first argument of `require` evaluates
-        // to `false`, execution terminates and all
-        // changes to the state and to Ether balances
-        // are reverted.
-        // This used to consume all gas in old EVM versions, but
-        // not anymore.
-        // It is often a good idea to use `require` to check if
-        // functions are called correctly.
-        // As a second argument, you can also provide an
-        // explanation about what went wrong.
-        require(
-            msg.sender == chairperson,
-            "Only chairperson can give right to vote."
+    // Create a proposal to add or remove a Gnosis Safe owner
+    function createProposal(
+        string memory _name,
+        address _candidate,
+        bool _willBeOwner
+    ) public {
+        proposals.push(
+            Proposal({
+                name: _name,
+                candidate: _candidate,
+                willBeOwner: _willBeOwner,
+                voteCount: 0,
+                executed: false
+            })
         );
-        require(!voters[voter].voted, "The voter already voted.");
-        require(voters[voter].weight == 0);
-        voters[voter].weight = 1;
     }
 
-    /// Delegate your vote to the voter `to`.
-    function delegate(address to) external {
-        // assigns reference
-        Voter storage sender = voters[msg.sender];
-        require(!sender.voted, "You already voted.");
+    function getWeight(address voter) public returns (uint256) {}
 
-        require(to != msg.sender, "Self-delegation is disallowed.");
+    /// Vote on a given proposal.
+    function vote(uint256 proposalId) external {
+        address sender = msg.sender;
+        require(voters[sender].weight > 0, "Already voted");
+        require(proposalId < proposals.length, "Invalid proposal");
 
-        // Forward the delegation as long as
-        // `to` also delegated.
-        // In general, such loops are very dangerous,
-        // because if they run too long, they might
-        // need more gas than is available in a block.
-        // In this case, the delegation will not be executed,
-        // but in other situations, such loops might
-        // cause a contract to get "stuck" completely.
-        while (voters[to].delegate != address(0)) {
-            to = voters[to].delegate;
+        uint256 weight = getWeight(sender);
+        require(weight > 0, "Has no right to vote");
 
-            // We found a loop in the delegation, not allowed.
-            require(to != msg.sender, "Found loop in delegation.");
-        }
-
-        // Since `sender` is a reference, this
-        // modifies `voters[msg.sender].voted`
-        Voter storage delegate_ = voters[to];
-
-        // Voters cannot delegate to wallets that cannot vote.
-        require(delegate_.weight >= 1);
-        sender.voted = true;
-        sender.delegate = to;
-        if (delegate_.voted) {
-            // If the delegate already voted,
-            // directly add to the number of votes
-            proposals[delegate_.vote].voteCount += sender.weight;
-        } else {
-            // If the delegate did not vote yet,
-            // add to her weight.
-            delegate_.weight += sender.weight;
-        }
-    }
-
-    /// Give your vote (including votes delegated to you)
-    /// to proposal `proposals[proposal].name`.
-    function vote(uint256 proposal) external {
-        Voter storage sender = voters[msg.sender];
-        require(sender.weight != 0, "Has no right to vote");
-        require(!sender.voted, "Already voted.");
-        sender.voted = true;
-        sender.vote = proposal;
-
-        // If `proposal` is out of the range of the array,
-        // this will throw automatically and revert all
-        // changes.
-        proposals[proposal].voteCount += sender.weight;
+        voters[sender].weight = weight;
+        voters[sender].vote = proposalId;
+        proposals[proposalId].voteCount += weight;
     }
 
     /// @dev Computes the winning proposal taking all
-    /// previous votes into account.
-    function winningProposal() public view returns (uint256 winningProposal_) {
+    /// previous votes into account and executes it.
+    function determineAndExecuteWinningProposal() public {
         uint256 winningVoteCount = 0;
+        uint256 winningProposal = 0;
         for (uint256 p = 0; p < proposals.length; p++) {
-            if (proposals[p].voteCount > winningVoteCount) {
+            if (
+                proposals[p].voteCount > winningVoteCount &&
+                !proposals[p].executed
+            ) {
                 winningVoteCount = proposals[p].voteCount;
-                winningProposal_ = p;
+                winningProposal = p;
             }
         }
+        require(winningVoteCount > 0, "Winning proposal needs at least 1 vote");
+        require(
+            !proposals[winningProposal].executed,
+            "Proposal already executed"
+        );
+        _executeProposal(winningProposal);
     }
 
-    // Calls winningProposal() function to get the index
-    // of the winner contained in the proposals array and then
-    // returns the name of the winner
-    function winnerName() external view returns (bytes32 winnerName_) {
-        winnerName_ = proposals[winningProposal()].name;
+    /// @dev Executes a proposal (only called by `executeWinningProposal`)
+    function _executeProposal(uint256 proposalId) private {
+        GnosisSafe _gnosisSafe = GnosisSafe(gnosisSafe);
+
+        if (proposals[proposalId].willBeOwner) {
+            _gnosisSafe.addOwnerWithThreshold(
+                proposals[proposalId].candidate,
+                _gnosisSafe.getThreshold()
+            );
+        } else {
+            _gnosisSafe.removeOwner(
+                address(this),
+                proposals[proposalId].candidate,
+                _gnosisSafe.getThreshold()
+            );
+        }
     }
 }
